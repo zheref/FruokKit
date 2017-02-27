6574a09a723782259dacd93c63d67c105fbf8ad3
Sergio Daniel Lozano <zheref@icloud.com>
2017-02-27 12:47:04 -0500

    Add macOS target

---
+++ FruokKit macOS/FruokKit macOS.h
@@ -0,0 +1,19 @@
+//
+//  FruokKit macOS.h
+//  FruokKit macOS
+//
+//  Created by Sergio Daniel L. García on 2/27/17.
+//  Copyright © 2017 Sergio Daniel L. García. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for FruokKit macOS.
+FOUNDATION_EXPORT double FruokKit_macOSVersionNumber;
+
+//! Project version string for FruokKit macOS.
+FOUNDATION_EXPORT const unsigned char FruokKit_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FruokKit_macOS/PublicHeader.h>
+
+

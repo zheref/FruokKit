a7d729d6bc41c3e7ab23888e3b6a90dde5a757d2
Sergio Daniel Lozano <zheref@icloud.com>
2017-02-27 12:44:19 -0500

    Initial Commit

---
+++ FruokKit/FruokKit.h
@@ -0,0 +1,19 @@
+//
+//  FruokKit.h
+//  FruokKit
+//
+//  Created by Sergio Daniel L. García on 2/27/17.
+//  Copyright © 2017 Sergio Daniel L. García. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FruokKit.
+FOUNDATION_EXPORT double FruokKitVersionNumber;
+
+//! Project version string for FruokKit.
+FOUNDATION_EXPORT const unsigned char FruokKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FruokKit/PublicHeader.h>
+
+

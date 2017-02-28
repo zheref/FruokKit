23afbcaa967ddbd5feaf6ca3dfcac677794ffefd
Sergio Daniel Lozano <zheref@icloud.com>
2017-02-28 06:50:47 -0500

    Add first view model to refence on app

---
+++ FruokKit/NewProjectModel.swift
@@ -0,0 +1,28 @@
+//
+//  NewProjectModel.swift
+//  FruokKit
+//
+//  Created by Sergio Daniel L. García on 2/28/17.
+//  Copyright © 2017 Sergio Daniel L. García. All rights reserved.
+//
+
+import Foundation
+
+
+public class NewProjectModel {
+    
+    // MARK: - PROPERTIES
+    
+    /**
+     * The codename of the project itself
+     **/
+    var codename = String()
+    
+    /**
+     * The commercial name of the project
+     **/
+    var name = String()
+    
+    public init() { }
+    
+}
